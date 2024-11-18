a1e4cecf13308f09c97ba3319d01306cc0c75998
EslamKamel89 <elsamkamelforex@gmail.com>
2024-11-18 17:40:23 +0200

    creating the logic for logout

--- lib/pages/profile/controller.dart
+++ lib/pages/profile/controller.dart
@@ -1,8 +1,24 @@
 import 'package:chatty_app/pages/profile/state.dart';
+import 'package:chatty_app/utils/helpers/print_helper.dart';
+import 'package:chatty_app/utils/store/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ProfileController extends GetxController {
   ProfileController();
 
   final ProfileState state = ProfileState();
+  Future<void> signOut() async {
+    const t = 'signOut - ProfileController';
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    try {
+      await googleSignIn.signOut();
+      await FirebaseAuth.instance.signOut();
+      await UserStore.to.onLogout();
+      pr('User logged out successfully');
+    } catch (e) {
+      pr('Exception occured: $e', t);
+    }
+  }
 }
--- lib/pages/profile/view/view.dart
+++ lib/pages/profile/view/view.dart
@@ -1,6 +1,7 @@
 import 'package:chatty_app/pages/profile/index.dart';
 import 'package:chatty_app/pages/profile/view/widgets/user_profile_photo.dart';
 import 'package:chatty_app/utils/values/colors.dart';
+import 'package:chatty_app/utils/widgets/sizer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -17,11 +18,36 @@ class ProfilePage extends GetView<ProfileController> {
           slivers: [
             SliverToBoxAdapter(
               child: Container(
-                  child: const Column(
-                children: [
-                  UserProfilePhoto(),
-                ],
-              )),
+                child: Column(
+                  children: [
+                    const UserProfilePhoto(),
+                    const Sizer(),
+                    MainButton(
+                      onTap: () {},
+                      content: 'Complete',
+                    ),
+                    const Sizer(),
+                    MainButton(
+                      onTap: () {
+                        Get.defaultDialog(
+                          title: 'Are You Sure!!!',
+                          onConfirm: () async {
+                            await controller.signOut();
+                            // Get.
+                          },
+                          onCancel: () {},
+                          textConfirm: 'Confirm',
+                          confirmTextColor: Colors.white,
+                          textCancel: 'Cancel',
+                          content: const SizedBox(),
+                        );
+                      },
+                      content: 'Log Out',
+                      color: AppColors.primarySecondaryElementText,
+                    ),
+                  ],
+                ),
+              ),
             )
           ],
         ),
@@ -38,6 +64,51 @@ class ProfilePage extends GetView<ProfileController> {
           color: AppColors.primaryText,
         ),
       ),
+    );
+  }
+}
+
+class MainButton extends StatelessWidget {
+  const MainButton({
+    super.key,
+    required this.onTap,
+    required this.content,
+    this.color,
+  });
+  final void Function()? onTap;
+  final String content;
+  final Color? color;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          width: 295.w,
+          height: 44.h,
+          decoration: BoxDecoration(
+            color: color ?? AppColors.primaryElement,
+            borderRadius: BorderRadius.circular(5.w),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                content,
+                style: TextStyle(
+                  color: AppColors.primaryElementText,
+                  fontSize: 14.sp,
+                ),
+              )
+            ],
+          )),
     );
   }
 }
